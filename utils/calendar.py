e650828437198d17bb97925998d0388bf6dd7623
Parker Wahle <parkeredwardwahle2017@gmail.com>
2022-04-08 08:41:04 -0400

    Respect timezone

--- utils/calendar.py
+++ utils/calendar.py
@@ -1,19 +1,40 @@
 from asyncio import get_event_loop, AbstractEventLoop
+from datetime import tzinfo
 
 from ics import *
+from ics.parse import ContentLine
+from pytz import UTC
 from saturnscrape import *
+from tzlocal import get_localzone
 
 
 async def make_calendar(client: SaturnLiveClient, school_id: str, student_id: int) -> Calendar:
     """Make an ICS calendar from a calendar on https://saturn.live."""
+
+    calendar_days: list[CalendarDay] = await client.get_calendar(school_id)
+    schedules: list[BellSchedule] = await client.get_schedules(student_id, include_chats=False)
+    student: Student = await client.get_student(student_id)
+
+    school_name: str = school_id.replace('-', ' ').title()
+
+    if isinstance(student, FullStudent):
+        school_name = student.school_title
+
     loop: AbstractEventLoop = get_event_loop()
     calendar: Calendar = await loop.run_in_executor(
         None,
         lambda: Calendar(creator=str(f"saturn-{school_id}-{student_id}"))
     )
+    tz: tzinfo = get_localzone()
 
-    calendar_days: list[CalendarDay] = await client.get_calendar(school_id)
-    schedules: list[BellSchedule] = await client.get_schedules(student_id, include_chats=False)
+    calendar.extra.extend(
+        [
+            ContentLine('X-WR-CALNAME', value=f"Saturn {school_name} Schedule"),
+            ContentLine('X-WR-TIMEZONE', value=str(tz)),
+            ContentLine('X-WR-CALDESC', value=f"Saturn {school_name} Schedule"),
+            ContentLine("COLOR", value="#7126f8"),
+        ]
+    )
 
     for day in calendar_days:
         if day.schedule and not day.is_canceled:
@@ -30,8 +51,12 @@ async def make_calendar(client: SaturnLiveClient, school_id: str, student_id: in
 
                     class_event: Event = Event(
                         name=f"Period {period.name} - {def_course.nickname or course.name} - {def_course.room}",
-                        begin=period.start_time.replace(year=day.date.year, month=day.date.month, day=day.date.day),
-                        end=period.end_time.replace(year=day.date.year, month=day.date.month, day=day.date.day),
+                        begin=period.start_time.replace(
+                            year=day.date.year, month=day.date.month, day=day.date.day, tzinfo=tz
+                        ).astimezone(UTC),
+                        end=period.end_time.replace(
+                            year=day.date.year, month=day.date.month, day=day.date.day, tzinfo=tz
+                        ).astimezone(UTC),
                         attendees=[
                             Attendee(common_name=student.name, email=student.email) for student in def_course.classmates
                         ],
@@ -43,8 +68,12 @@ async def make_calendar(client: SaturnLiveClient, school_id: str, student_id: in
                     calendar.events.add(
                         Event(
                             name=period.name,
-                            begin=period.start_time.replace(year=day.date.year, month=day.date.month, day=day.date.day),
-                            end=period.end_time.replace(year=day.date.year, month=day.date.month, day=day.date.day),
+                            begin=period.start_time.replace(
+                                year=day.date.year, month=day.date.month, day=day.date.day, tzinfo=tz
+                            ).astimezone(UTC),
+                            end=period.end_time.replace(
+                                year=day.date.year, month=day.date.month, day=day.date.day, tzinfo=tz
+                            ).astimezone(UTC),
                         )
                     )
 
