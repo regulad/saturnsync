bdf455eded806fb0dd07dcd122a484212ef5b10b
Parker Wahle <parkeredwardwahle2017@gmail.com>
2022-04-09 20:29:30 -0400

    Added contacts

--- routes.py
+++ routes.py
@@ -1,9 +1,12 @@
 from asyncio import get_running_loop
 
 from aiohttp import web
+from aiohttp.web_exceptions import HTTPBadRequest
 from ics import Calendar
+from saturnscrape import Student
 
-from saturnscrape.utils import make_calendar
+from saturnscrape.utils import make_calendar, make_contact
+from vobject.base import Component
 
 ROUTES: web.RouteTableDef = web.RouteTableDef()
 
@@ -17,6 +20,19 @@ async def get_calendar_endpoint(request: web.Request):
         text=await get_running_loop().run_in_executor(None, lambda: str(calendar)),
         content_type="text/calendar",
     )
+
+
+@ROUTES.get("/{student_id}.vcf")
+async def get_contact_endpoint(request: web.Request):
+    if request.match_info["student_id"] == "me":
+        raise HTTPBadRequest(reason="You cannot access me")
+    else:
+        student: Student = await request.app["client"].get_student(request.match_info["student_id"])
+        contact: Component = await get_running_loop().run_in_executor(None, lambda: make_contact(student))
+        return web.Response(
+            text=await get_running_loop().run_in_executor(None, lambda: contact.serialize()),
+            content_type="text/vcard",
+        )
 
 
 __all__ = ["ROUTES"]
